4c1d15b80f5a88e2071162563f740a74ace1eef0
Lurian Ortiz <lurian502@gmail.com>
2023-02-15 22:13:15 -0300

    Alteraçãoes na componente cesta

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <StatusBar style="auto" />
+  return ( 
+     <SafeAreaView>    
+      <StatusBar />
       <Cesta />
     </SafeAreaView>
   );
 }
-
+// o SafeAreaView cria como se fosse uma "margem"
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,29 +1,78 @@
-import { Image, StyleSheet, Dimensions, Text } from 'react-native';
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
 import imagem from '../../assets/topo.png'
+import imagemFazendeiro from '../../assets/logo.png'
 
-const width = Dimensions.get('screen').width;
+const width = Dimensions.get('screen').width; // O Dimensions verifica qual é o tamnho da tela
 
 export default function Cesta(){
-    return(
-        <>
+    return <>
             <Image style={styles.topo} source={imagem} />
             <Text style={styles.titulo}>Detalhes da Cesta</Text>
+
+            <View style={styles.cesta}>
+                <Text style={styles.nome}>Cesta de verduras</Text>
+                <View style={styles.sessaoFazendeiro}>
+                    <Image source={imagemFazendeiro} style={styles.imagemLogoFazendeiro}/>
+                    <Text style={styles.nomeFazendeiro}>Jenny Jack Farm</Text> 
+                </View>
+                <Text style={styles.descricao}>
+                    Uma cesta com produtos selecionados
+                    cuidadosamente da fazenda direto para sua 
+                    cozinha
+                </Text>
+                <Text style={styles.preco}>R$ 40,00</Text>
+            </View>
         </> /*nome disso é um fragment*/
-    );
+    
 }
 
 const styles = StyleSheet.create({
     topo: {
         width: '100%',
-        height: 578 / 768 * width,
+        height: 578 / 768 * width, //578 é a altura da imagem e 768 é a largura da imagem
     },
     titulo: {
         width: "100%",
-        position: "absolute",
+        position: "absolute", 
         textAlign: "center",
-        fontSize: "16",
         color: "#fff", 
         fontWeight: "bold",
-        padding: 16
+        padding: 16,
+        fontSize: 16
+    },
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    sessaoFazendeiro: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemLogoFazendeiro:{
+        width: 32,
+        height: 32
+    },
+    nomeFazendeiro: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 10
+    },
+    descricao: {
+        color:"#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco:{
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
